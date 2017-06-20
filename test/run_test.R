ff988364adde64703b87c20c7a7d1ce8ee484ec9
mpalerme <mpalerme@inra.fr>
2017-06-20 14:03:42 +0200

    add test for model.R

--- test/run_test.R
+++ test/run_test.R
@@ -3,6 +3,7 @@
 require('RUnit')
 
 source("src/landscape.R")
+source("src/model.R")
 
 test.suite <- defineTestSuite("tests",
                               dirs = "./test/unit",
@@ -12,3 +13,12 @@ test.result <- runTestSuite(test.suite)
 
 printTextProtocol(test.result)
 printJUnitProtocol(test.result,"./result/landscape.xml")
+
+test.suite <- defineTestSuite("tests",
+                              dirs = "./test/unit",
+                              testFileRegexp = '^.+\\model.R$')
+
+test.result <- runTestSuite(test.suite)
+
+printTextProtocol(test.result)
+printJUnitProtocol(test.result,"./result/model.xml")
